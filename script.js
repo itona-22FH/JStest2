870c989bdd2ce399f30ce4031adb074585b479d9
itona-22FH <naov7700@gmail.com>
2022-02-24 22:03:30 +0900

    JavaScript課題2(修正)

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
 'use strict';
 
 const execution = document.getElementById('execution');
-const output = document.getElementById('output');
+const fizzBuzzList = document.getElementById('fizzBuzzList');
 
-const addElement = text => {
-  const newDiv = document.createElement('div');
+const addLiElement = text => {
+  const newLi = document.createElement('li');
   const newText = document.createTextNode(text);
-  newDiv.appendChild(newText);
-  output.insertAdjacentElement('beforeend', newDiv);
-};
-
-const overWriteBlank = element => {
-  element.innerHTML = '';
+  newLi.appendChild(newText);
+  fizzBuzzList.insertAdjacentElement('beforeend', newLi);
 };
 
 execution.addEventListener('click', () => {
   const fizzNum = Number(document.getElementById('fizzNum').value);
   const buzzNum = Number(document.getElementById('buzzNum').value);
 
-  if (Number.isInteger(fizzNum) && Number.isInteger(buzzNum)) {
-    overWriteBlank(output);
+  if (
+    Number.isInteger(fizzNum) &&
+    Number.isInteger(buzzNum) &&
+    fizzNum &&
+    buzzNum
+  ) {
+    fizzBuzzList.innerHTML = '';
     for (let i = 1; i < 100; i++) {
       if (i % fizzNum === 0 && i % buzzNum === 0) {
-        addElement(`FizzBuzz ${i}`);
+        addLiElement(`FizzBuzz ${i}`);
       } else if (i % fizzNum === 0) {
-        addElement(`Fizz ${i}`);
+        addLiElement(`Fizz ${i}`);
       } else if (i % buzzNum === 0) {
-        addElement(`Buzz ${i}`);
+        addLiElement(`Buzz ${i}`);
       }
     }
-  } else if (!Number.isInteger(fizzNum) || !Number.isInteger(buzzNum)) {
-    overWriteBlank(output);
-    addElement('整数値を入力してください');
+  } else {
+    fizzBuzzList.innerHTML = '';
+    addLiElement('整数値を入力してください');
   }
 });
